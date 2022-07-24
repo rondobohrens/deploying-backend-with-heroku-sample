b9900f377c0418664d1b2387fe85cb8e1d9e58f8
rondobohrens <67055752+rondobohrens@users.noreply.github.com>
2022-07-24 13:22:59 +0200

    changed https to http as cert of page has expired

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch'); // import node-fetch (enables the fetch API
 const PORT = process.env.PORT || 5000; // use either the host env var port (PORT) provided by Heroku or the local port (5000) on your machine
 
 app.get('/', (req, res) => { // send a get request to root directory ('/' is this file (app.js))
-  fetch('https://www.boredapi.com/api/activity') // fetch activity from bored API - https://www.boredapi.com/about
+  fetch('http://www.boredapi.com/api/activity') // fetch activity from bored API - https://www.boredapi.com/about
     .then(res => res.json()) // return a promise containing the response
     .then(json => res.send(`<h1>Today's Activity: ${json.activity}!</h1>`)) // extract the JSON body content from the response (specifically the activity value) and sends it to the client
     .catch(function(err){ // catch any errors
